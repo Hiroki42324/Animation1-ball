faae1620f05317c5be35c7b76a2612ece7b9f69f
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-11-08 10:04:08 +0900

    version-3

--- animation1-ball.js
+++ animation1-ball.js
@@ -9,6 +9,7 @@ let dx=1.0; //x方向の移動量
 let dy=1.0; //y方向の移動量
 let g = 3.0; //重力加速度 px/s^2 
 let e = 0.6; //壁の反発係数
+let count = 0; //クリックカウント
                                  
 
 function draw(){
@@ -39,6 +40,30 @@ function drawCircle(x, y) {
     cc.restore();
 }
 
+// ボールがクリックされたかの判例処理 
+function clickCircle(mx, my) {
+// クリックした座標と、◯の現在位置(中心座標)との距離を計算
+let distance = Math.sqrt(
+    Math.pow(mx - x, 2) + Math.pow(my - y, 2));
+    // 距離がボールの半径以下かどうか 
+    if(distance <= r)return true;
+    return false; 
+}
+
+function onMouseClick(e) {
+    //クリックされた座標取得
+    const rect = canvas.getBoundingClientRect(); // Canvasの位置を取得 
+    let mx = e.clientX - rect.left; // クリックされたX座標
+    let my = e.clientY - rect.top; // クリックされたY座標
+    if(clickCircle(mx, my)) { // ボールをクリックできたかを判定 
+        // クリックされたカウントをインクリメント
+        count++;
+        // クリックのカウントを画面表示 
+        document.getElementById("count").innerHTML = count;
+    } 
+}
+
+
 function init(){
     canvas = document.getElementById("canvas"); 
     cc = canvas.getContext("2d"); //描画関数を一定間隔で繰り返し呼び出す
@@ -48,6 +73,8 @@ function init(){
 function main(){
     // 初期化呼び出し
     init();
+    // イベント設定
+    canvas.addEventListener('click', onMouseClick);
 }
 
 main();
