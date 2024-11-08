b213a67bf385748c7251e3003ca3ba9ab306f9e9
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-11-08 10:24:25 +0900

    version-4

--- animation1-ball.js
+++ animation1-ball.js
@@ -10,6 +10,7 @@ let dy=1.0; //y方向の移動量
 let g = 3.0; //重力加速度 px/s^2 
 let e = 0.6; //壁の反発係数
 let count = 0; //クリックカウント
+let drag = false; //マウスでドラッグ中を判定するフラグ
                                  
 
 function draw(){
@@ -20,6 +21,7 @@ function draw(){
 }
 
 function updateCoordinate() {
+    if (drag) return; // ドラッグ中は自動で移動させない
     dy = dy + g * interval;//重力方向の移動量(速度)を更新
     if((x + dx > canvas.width - r) || (x + dx < r)) {
         dx = (dx * -1) * e; //ベクトルを逆方向へ変換(反射係数を加味) 
@@ -63,6 +65,42 @@ function onMouseClick(e) {
     } 
 }
 
+function onMouseMove(e) { //指定された要素の上をマウスが移動している間実行される
+    if(!drag) return; //ドラッグ中でなければ以降の処理はしない
+    //今現在のマウスの座標
+    const rect = canvas.getBoundingClientRect(); // Canvasの位置を取得
+    let mx = e.clientX - rect.left; 
+    let my = e.clientY - rect.top;
+    //移動量の更新 
+    dx = mx - x; 
+    dy = my - y; 
+    x = mx;
+    y = my;
+}
+
+function onMouseDown(e) { //マウスクリック(押し込んだ時)
+    //クリックされた座標取得
+    const rect = canvas.getBoundingClientRect(); // Canvasの位置を取得 
+    let mx = e.clientX - rect.left; // クリックされたX座標 
+    let my = e.clientY - rect.top; // クリックされたY座標
+    if(clickCircle(mx,my)) {
+        drag = true; // ドラッグされている状態に変更 
+        x = mx; // ボールの座標を更新
+        y = my;
+        dx = 0; // 移動量は0でリセット
+        dy = 0;
+    } 
+}
+
+function onMouseUp(e) { //マウスクリック(クリック状態から離したとき) 
+    drag=false;
+}
+
+function onMouseOut(e) {
+    //指定された要素からマウスカーソルが離れた時
+    drag=false;
+}
+
 
 function init(){
     canvas = document.getElementById("canvas"); 
@@ -74,7 +112,11 @@ function main(){
     // 初期化呼び出し
     init();
     // イベント設定
-    canvas.addEventListener('click', onMouseClick);
+    // canvas.addEventListener('click', onMouseClick);
+    canvas.addEventListener("mousemove", onMouseMove); 
+    canvas.addEventListener("mousedown", onMouseDown); 
+    canvas.addEventListener("mouseup", onMouseUp); 
+    canvas.addEventListener("mouseout", onMouseOut);
 }
 
 main();
