0e37bd5e4027ae4a69ecc1264f2f81b562dbc90e
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-11-08 11:07:54 +0900

    version-5

--- animation1-ball.js
+++ animation1-ball.js
@@ -11,6 +11,10 @@ let g = 3.0; //重力加速度 px/s^2
 let e = 0.6; //壁の反発係数
 let count = 0; //クリックカウント
 let drag = false; //マウスでドラッグ中を判定するフラグ
+let reflectCount = 0; //反射した回数のカウント 
+let gameTimer; //タイマーID
+let gameTime = 10; //ゲーム時間10s
+let countdown; //カウントダウン
                                  
 
 function draw(){
@@ -25,9 +29,13 @@ function updateCoordinate() {
     dy = dy + g * interval;//重力方向の移動量(速度)を更新
     if((x + dx > canvas.width - r) || (x + dx < r)) {
         dx = (dx * -1) * e; //ベクトルを逆方向へ変換(反射係数を加味) 
+        reflectCount++;
+        document.getElementById("info").innerHTML = reflectCount;
     }
     if((y + dy >canvas.height -r) || (y + dy < r)) {
         dy = (dy * -1) * e; //ベクトルを逆方向へ変換(反射係数を加味)
+        reflectCount++;
+        document.getElementById("info").innerHTML = reflectCount;
     }
     x = x + dx;
     y = y + dy;
@@ -101,11 +109,51 @@ function onMouseOut(e) {
     drag=false;
 }
 
+function gameOver(){
+    //アニメーションの停止
+    clearInterval(timer);
+    //マウス操作イベントも停止 
+    canvas.removeEventListener("mousemove", onMouseMove); 
+    canvas.removeEventListener("mousedown", onMouseDown); 
+    canvas.removeEventListener("mouseup", onMouseUp); 
+    canvas.removeEventListener("mouseout", onMouseOut); //得点のハイライト表示 
+    document.getElementById("info").style.color = "red";
+}
+
+function drawGrid(){ 
+    cc.save();
+    cc.strokeStyle = "black";
+    cc.lineWidth = 0.5;
+    for(let i = 0; i < canvas.width; i += 20) {
+        cc.beginPath();
+        cc.moveTo(i, 0);
+        cc.lineTo(i, canvas.height);
+        cc.closePath();
+        cc.stroke();
+    }
+    for(let i = 0; i < canvas.height; i += 20) {
+        cc.beginPath();
+        cc.moveTo(0, i);
+        cc.lineTo(canvas.width, i);
+        cc.closePath();
+        cc.stroke();
+    }
+    cc.restore();
+}
+
 
 function init(){
+    document.getElementById("countdown").innerHTML = gameTime;
     canvas = document.getElementById("canvas"); 
     cc = canvas.getContext("2d"); //描画関数を一定間隔で繰り返し呼び出す
     timer = setInterval(draw, interval * 1000);
+    gameTimer = setTimeout(gameOver, gameTime * 1000);
+    gameTime -= 1;
+    countdown = setInterval(function(){
+        if(gameTime < 0)return;
+        document.getElementById("countdown").innerHTML = gameTime;
+        gameTime--;
+    }, 1000);
 }
 
 function main(){
@@ -117,6 +165,7 @@ function main(){
     canvas.addEventListener("mousedown", onMouseDown); 
     canvas.addEventListener("mouseup", onMouseUp); 
     canvas.addEventListener("mouseout", onMouseOut);
+    document.getElementById("start").addEventListener("click", init);
 }
 
 main();
