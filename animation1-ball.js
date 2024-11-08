247464319875533c33eadaa027ef16de2f550dcf
x22081xx <x22081xx@x22081xxnoMacBook-Air.local>
2024-11-08 09:47:33 +0900

    version-2

--- animation1-ball.js
+++ animation1-ball.js
@@ -7,15 +7,25 @@ let x=100;  //x座標
 let y=100;  //y座標
 let dx=1.0; //x方向の移動量 
 let dy=1.0; //y方向の移動量
+let g = 3.0; //重力加速度 px/s^2 
+let e = 0.6; //壁の反発係数
                                  
 
 function draw(){
     //canvasのクリア
-    cc.clearRect(0, 0, canvas.width, canvas.height); drawCircle(x,y);
+    cc.clearRect(0, 0, canvas.width, canvas.height); 
+    drawCircle(x,y);
     updateCoordinate();
 }
 
 function updateCoordinate() {
+    dy = dy + g * interval;//重力方向の移動量(速度)を更新
+    if((x + dx > canvas.width - r) || (x + dx < r)) {
+        dx = (dx * -1) * e; //ベクトルを逆方向へ変換(反射係数を加味) 
+    }
+    if((y + dy >canvas.height -r) || (y + dy < r)) {
+        dy = (dy * -1) * e; //ベクトルを逆方向へ変換(反射係数を加味)
+    }
     x = x + dx;
     y = y + dy;
 }
